fc1741f5d88d37a73623f868fe3474e1596e9be9
Jason Jeffrey <jason@jjeffrey.net>
2014-07-05 21:37:08 +0100

    wip: githooks

--- libs/githooks.js
+++ libs/githooks.js
@@ -3,10 +3,25 @@
 var fs = require('fs');
 
 exports.execute = function () {
-    var installedProjects = fs.readFileSync(__dirname + '/../config.json').projects;
+    var installedProjects = fs.readFileSync(__dirname + '/../config.json').projects,
+        currentProject = process.cwd();
 
-    //get current directory
-    console.log(process.cwd());
+    if(installedProjects.indexOf(currentProject) !== -1) {
+        console.log('ERROR: Project already has githooks installed. use bur update to get the latest version');
+        process.exit(1);
+    }
+
+    if(fs.existsSync(currentProject + '/.git')) {
+        console.log('ERROR: GIT folder does not exist in this project');
+        process.exit(1);
+    }
+
+    //get hooks from github
+
+    //add them to git hooks folder
+
+    //add project to the config.json
+
 }
 
 exports.help = function () {
