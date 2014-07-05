a4592c2d8eba5b0d1bd838f18882a6183a585d9c
Jason Jeffrey <jason@jjeffrey.net>
2014-07-05 20:56:19 +0100

    added grunt

--- libs/help.js
+++ libs/help.js
@@ -1,8 +1,30 @@
 'use strict';
 
+var fs = require('fs');
+
 exports.execute = function () {
     // for each file on libs call the help function within them
-    console.log('Burrows commandline Tool \n================\nadd any of the following after the bur command to run:');
-    process.exit(0);
+    fs.readdir(__dirname, function (err, files) {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+
+        console.log('\n================\nBurrows Commandline Tool \n================\n' +
+            'add any of the following after the bur command to run:\n');
+
+        files.forEach(function (file) {
+            var command = require(__dirname + '/' + file);
+
+            command.help();
+        });
+
+        console.log('');
+
+        process.exit(0);
+    });
 }
 
+exports.help = function () {
+    console.log("help - shows all available commands");
+}
