4fcfde6496c7788065e9c9a108fa82e5accec45e
Jason Jeffrey <jason@jjeffrey.net>
2014-07-05 19:27:16 +0100

    added the help file and basic functionality to the bin file

---
+++ libs/help.js
@@ -0,0 +1,8 @@
+'use strict';
+
+exports.execute = function () {
+    // for each file on libs call the help function within them
+    console.log('Burrows commandline Tool \n================\nadd any of the following after the bur command to run:');
+    process.exit(0);
+}
+
