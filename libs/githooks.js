41d7c4a97f5a0e026dba8e6e94a377fcb1a24f85
Jason Jeffrey <jason.jeffrey@burrows.yr.com>
2014-07-08 17:26:41 +0100

    completed one path

--- libs/githooks.js
+++ libs/githooks.js
@@ -2,6 +2,8 @@
 
 var fs = require('fs'),
     https = require('https'),
+    ncp = require('ncp').ncp,
+    exec = require('child_process').exec,
     githubApiUrl = 'api.github.com',
     gitApiHttpsOptions = {
         'hostname': githubApiUrl,
@@ -11,7 +13,12 @@ var fs = require('fs'),
     };
 
 exports.execute = function () {
-    var installedProjects = fs.readFileSync(__dirname + '/../config.json').projects,
+    var githubProjectData,
+        cachePath = __dirname + '/../cache',
+        githooksPath = cachePath + '/githooks',
+        config = JSON.parse(fs.readFileSync(__dirname + '/../config.json', {encoding: 'utf8'})),
+        installedProjects = config.projects,
+        githooksCacheVersion = config.cache.githooks,
         currentProject = process.cwd();
 
     if(installedProjects && installedProjects.indexOf(currentProject) !== -1) {
@@ -36,11 +43,51 @@ exports.execute = function () {
         });
 
         githubResponse.on('end', function () {
-           console.log(data);
-        });
-    })
+            data = JSON.parse(data);
+            githubProjectData = data.filter(function (item) {
+                return item.name === 'master';
+            })[0];
 
-    //add them to git hooks folder
+            if(!githooksCacheVersion) {
+                if(!fs.existsSync(cachePath)) {
+                    fs.mkdirSync(cachePath);
+                }
+
+                if(!fs.existsSync(githooksPath)) {
+                    fs.mkdirSync(githooksPath);
+                }
+
+                exec('git clone -b master https://github.com/burrows-codefest/git-hooks.git ' + githooksPath,
+                function (error) {
+                    if (error) {
+                        console.log('ERROR: GIT Clone failed - ' + error);
+                        process.exit(1);
+                    }
+
+                    if(!fs.existsSync(currentProject + '/.git/hooks')) {
+                        fs.mkdirSync(currentProject + '/.git/hooks');
+                    }
+
+                    ncp(githooksPath + '/hooks', currentProject + '/.git/hooks', function (err) {
+                        if (err) {
+                            console.log('ERROR: File Copy failed - ' + err);
+                            process.exit(1);
+                        }
+
+                        config.cache.githooks = githubProjectData.commit.sha;
+                        config.projects[currentProject] = githubProjectData.commit.sha;
+                        console.log('git hooks updated for this project');
+                    });
+                });
+
+            } else if (githooksCacheVersion !== githubProjectData.commit.sha) {
+                //git pull project
+            } else {
+                console.log('This Project currently has the latest version of git hooks');
+                process.exit(0);
+            }
+        });
+    });
 
     //add project to the config.json
 
