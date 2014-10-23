9caf11126437da8b6103cd7c4684583edd866740
Jason Jeffrey <jason.jeffrey@burrows.yr.com>
2014-10-23 16:22:29 +0100

    update bin file to use commander

---
+++ bin/bur.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var program = require('commander'),
+    gitHooks = require('../libs/githooks'),
+    pkgJson = require('../package.json');
+
+program.version(pkgJson.version);
+
+program
+    .command('githooks')
+    .description('adds customised git hooks to the current project')
+    .action(gitHooks.execute());
+
+program.parse(process.argv);
