12fc0203a204be03b31865a99ac27454fce32267
Jason Jeffrey <jason.jeffrey@burrows.yr.com>
2014-10-24 10:33:49 +0100

    add coverage tests

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -1,5 +1,6 @@
 module.exports = function () {
     return {
-        default: ['jshint:all', 'mocha-chai-sinon:unit']
+        default: ['jshint:all', 'mocha-chai-sinon:unit'],
+        coverage: ['jshint:all', 'mocha-chai-sinon:coverage']
     }
 };
--- grunt/mocha-chai-sinon.js
+++ grunt/mocha-chai-sinon.js
@@ -7,5 +7,18 @@ module.exports = {
             ui: 'bdd',
             reporter: 'spec'
         }
+    },
+    coverage: {
+        src: ['test/unit/**/*Spec.js'],
+        options: {
+            ui: 'bdd',
+            reporter: 'html-cov',
+            quiet: true,
+            filter: [
+                '/libs/',
+                'bin'
+            ],
+            captureFile: './coverage.html'
+        }
     }
 };
